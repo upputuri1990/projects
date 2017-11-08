609760dd0088085d4b018de8bf6a5c88f3a09f8f
upputuri1990 <upputuri1990@gmail.com>
2017-11-07 19:23:43 -0800

    New file occurance by using treemap

--- coding/src/coding/OccuranceofEachLetter.java
+++ coding/src/coding/OccuranceofEachLetter.java
@@ -3,7 +3,7 @@ package coding;
 public class OccuranceofEachLetter {
 	public static void main(String[] args) {
 		String string="sanjaisahul";
-		char[] c=string.toCharArray();
+		/*char[] c=string.toCharArray();
 		int count=0;
 		for (char repetetive : c) {
 			if (repetetive=='s') {
@@ -15,8 +15,16 @@ public class OccuranceofEachLetter {
 		}
 		System.out.println("occurance of letter s is  "+count +"times");
 	}
-
+*/
+	
+	
+		
+	
+	
+	
+		
 		}
+}
 			
 
 
